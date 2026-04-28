f572590b1ad65896c466e2f082850bb95d1db3ad
ItamarHassin <itamar.hassin@gmail.com>
2026-04-28 08:44:58 +0300

    commenting
    Updated max pooling layers to reflect new output dimensions.

--- DistractionModel.py
+++ DistractionModel.py
@@ -39,18 +39,18 @@ class CNN_Distract(nn.Module):
             #first conv layer
             nn.Conv2d(3, 32, 3, padding=1, bias=False), nn.BatchNorm2d(32), nn.ReLU(inplace=True),
             nn.Conv2d(32, 32, 3, padding=1, bias=False), nn.BatchNorm2d(32), nn.ReLU(inplace=True),
-            nn.MaxPool2d(2),  #112x112
+            nn.MaxPool2d(2),  #128x128
 
 
             #Second conv layer
             nn.Conv2d(32, 64, 3, padding=1, bias=False), nn.BatchNorm2d(64), nn.ReLU(inplace=True),
             nn.Conv2d(64, 64, 3, padding=1, bias=False), nn.BatchNorm2d(64), nn.ReLU(inplace=True),
-            nn.MaxPool2d(2),  #56x56
+            nn.MaxPool2d(2),  #64x64
 
             #Third conv layer
             nn.Conv2d(64, 128, 3, padding=1, bias=False), nn.BatchNorm2d(128), nn.ReLU(inplace=True),
             nn.Conv2d(128,128, 3, padding=1, bias=False), nn.BatchNorm2d(128), nn.ReLU(inplace=True),
-            nn.MaxPool2d(2),  #28x28
+            nn.MaxPool2d(2),  #32x32
 
             
             
